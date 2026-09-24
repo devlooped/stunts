339c9df779c86e4866f74bb6dbb2e704a99190f3
Daniel Cazzulino <daniel@cazzulino.com>
2026-09-24 15:38:33 -0300

    Replace custom polyfills with PolySharp
    Drop the hand-written IsExternalInit, Index/Range, and ModuleInitializerAttribute types. PolySharp 1.16.0 generates them for netstandard2.0 and net472.

--- src/Avatar.UnitTests/MSBuildInitializer.cs
+++ src/Avatar.UnitTests/MSBuildInitializer.cs
@@ -34,10 +34,4 @@ namespace Avatars.UnitTests
     }
 }
 
-namespace System.Runtime.CompilerServices
-{
-    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
-    public sealed class ModuleInitializerAttribute : Attribute { }
-}
-
 #endif
